0326dba892a5f693e540a1c820793c6e836dc4f9
Bhawan sharma <83630093+Alfacito@users.noreply.github.com>
2021-08-04 13:02:20 +0530

    commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
+
 from . import views
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +24,5 @@ urlpatterns = [
     path('analyze', views.analyze,name='analyze'),
     path('aboutus/',views.aboutus,name='aboutus'),
 
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ]
